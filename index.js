b9fbc1af6fc97a467d4b0c5491b9cb423e76dd4c
Michael O'Grady <mogrady.professional@gmail.com>
2023-06-08 18:32:02 +0100

    prep for vercel deploy

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express');
-
-const app = express();
-const PORT = 4000;
-
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
-});
+const express = require('express'); // Express web server framework
+const colors = require('colors'); // For pretty console output
+const dotenv = require('dotenv').config(); // Load environment variables from .env file
+const cors = require('cors'); // CORS middleware
+// Bring in Error Handler Middleware
+const { errorHandler } = require('./middleware/errorMiddleware');
+// Bring in Connection to MongoDB
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000; // Port to listen on
+// Connect to MongoDB
+connectDB();
+const app = express(); // Create a new Express application
+app.use(express.json()); // Parse request body as JSON
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
@@ -13,13 +19,36 @@ app.get('/', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   res.json({
-    message: 'Hello, World2!',
+    message: 'Hello, World3!',
   });
 });
 
 app.get('/about', (req, res) => {
   res.send('This is my about route..... ');
 });
+
+app.use(errorHandler);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// Retrieve the NODE_ENV value
+const nodeEnv = process.env.NODE_ENV;
+
+// Determine the environment based on NODE_ENV value
+const isProduction = nodeEnv === 'production';
+const isLocal = nodeEnv === 'development';
+
+// Example usage
+if (isProduction) {
+  console.log('Running in production environment');
+} else if (isLocal) {
+  console.log('Running in local environment');
+} else {
+  console.log('Unknown environment');
+}
 
 // Export the Express API
 module.exports = app;
